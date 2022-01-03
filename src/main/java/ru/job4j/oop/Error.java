7dd0dcddd5341fc101451605dd6b3dd85f75a585
alex3 <alexprocards@mail.ru>
2022-01-03 12:46:10 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+
+    private int status;
+
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 10000, "I see you");
+        error.printInfo();
+        Error second = new Error();
+        second.printInfo();
+    }
+}
