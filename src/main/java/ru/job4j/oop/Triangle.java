013ec450ad9b9569eee69b62fb5f493f662b3a98
alex3 <alexprocards@mail.ru>
2022-01-26 11:12:48 +0500

    2.2. Рефакторинг - Площадь треугольника.[#122642 #255294]

--- src/main/java/ru/job4j/oop/Triangle.java
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -3,9 +3,9 @@ package ru.job4j.oop;
 import static java.lang.Math.sqrt;
 
     public class Triangle {
-         private Point first;
-         private Point second;
-         private Point third;
+        private Point first;
+        private Point second;
+        private Point third;
 
         public Triangle(Point ap, Point bp, Point cp) {
             this.first = ap;
@@ -18,11 +18,11 @@ import static java.lang.Math.sqrt;
         }
 
         public boolean exist(double ab, double ac, double bc) {
-            return (ab + ac < bc && ab + bc < ac && ac + bc < ab);
+            return (ab + ac > bc && ab + bc > ac && ac + bc > ab);
         }
 
         public double area() {
-            double rsl = 0.1;
+            double rsl = -1;
             double ab = first.distance(second);
             double ac = first.distance(third);
             double bc = second.distance(third);
@@ -31,5 +31,14 @@ import static java.lang.Math.sqrt;
                 rsl = sqrt(p * (p - ab) * (p - ac) * (p - bc));
             }
             return rsl;
+        }
+
+        public static void main(String[] args) {
+            Point a = new Point(8, 5);
+            Point b = new Point(2, 1);
+            Point c = new Point(8, 1);
+            Triangle triangle = new Triangle(a, b, c);
+            double rsl = triangle.area();
+            System.out.println(rsl);
         }
     }
