fce163554cd2303a935fb5b6d80870e91fd9b60b
alex3 <alexprocards@mail.ru>
2022-02-01 02:03:40 +0500

    3. Реализация профессий в коде [#6837 #258629]

--- src/main/java/ru/job4j/inheritance/Profession.java
+++ src/main/java/ru/job4j/inheritance/Profession.java
@@ -2,13 +2,13 @@ package ru.job4j.inheritance;
 
 public class Profession {
 
-     private String name;
+    private String name;
 
-     private String surname;
+    private String surname;
 
-     private String education;
+    private String education;
 
-     private int birthday;
+    private int birthday;
 
     public Profession(String name, String surname, String education, int birthday) {
         this.name = name;
