2e576e1e3dc62d31db4214982fee0d4ac3adcbb6
alex3 <alexprocards@mail.ru>
2022-02-13 01:03:33 +0500

    Массивы и модели. [#174892 #262423]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -13,26 +13,26 @@ public class Library {
         books[1] = two;
         books[2] = three;
         books[3] = four;
-            for (int index = 0; index < books.length; index++) {
-            Book pg = books[index];
-            System.out.println(pg.getName() + " - " + pg.getPages());
+        for (int index = 0; index < books.length; index++) {
+        Book pg = books[index];
+        System.out.println(pg.getName() + " - " + pg.getPages());
         }
 
-            System.out.println("Переставьте местами книги с индексом 0 и 3");
-            Book temp = books[0];
-            books[0] = books[3];
-            books[3] = temp;
-            for (int index = 0; index < books.length; index++) {
-                Book pg = books[index];
-                System.out.println(pg.getName() + " - " + pg.getPages());
-            }
+        System.out.println("Переставьте местами книги с индексом 0 и 3");
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int index = 0; index < books.length; index++) {
+        Book pg = books[index];
+        System.out.println(pg.getName() + " - " + pg.getPages());
+        }
 
-            System.out.println("цикл с выводом книг с именем \"Clean code\"");
-            for (int index = 0; index < books.length; index++) {
-            Book pg = books[index];
-            if (pg.getName().equals("Clean code")) {
-                System.out.println(pg.getName() + " - " + pg.getPages());
-            }
+        System.out.println("цикл с выводом книг с именем \"Clean code\"");
+        for (int index = 0; index < books.length; index++) {
+        Book pg = books[index];
+        if ("Clean code".equals(pg.getName())) {
+        System.out.println(pg.getName() + " - " + pg.getPages());
         }
     }
+}
 }
