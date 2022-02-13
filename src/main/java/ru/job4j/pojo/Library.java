b832907af79831933281f8ddedbaae96c586683e
alex3 <alexprocards@mail.ru>
2022-02-13 22:58:03 +0500

    3. Массивы и модели. [#174892 #262423]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -13,22 +13,22 @@ public class Library {
         books[2] = three;
         books[3] = four;
         for (int index = 0; index < books.length; index++) {
-        Book pg = books[index];
-        System.out.println(pg.getName() + " - " + pg.getPages());
+            Book pg = books[index];
+            System.out.println(pg.getName() + " - " + pg.getPages());
         }
         System.out.println("Переставьте местами книги с индексом 0 и 3");
         Book temp = books[0];
         books[0] = books[3];
         books[3] = temp;
         for (int index = 0; index < books.length; index++) {
-        Book pg = books[index];
-        System.out.println(pg.getName() + " - " + pg.getPages());
+            Book pg = books[index];
+            System.out.println(pg.getName() + " - " + pg.getPages());
         }
         System.out.println("цикл с выводом книг с именем \"Clean code\"");
         for (int index = 0; index < books.length; index++) {
-        Book pg = books[index];
-        if ("Clean code".equals(pg.getName())) {
-        System.out.println(pg.getName() + " - " + pg.getPages());
+            Book pg = books[index];
+            if ("Clean code".equals(pg.getName())) {
+            System.out.println(pg.getName() + " - " + pg.getPages());
         }
     }
   }
