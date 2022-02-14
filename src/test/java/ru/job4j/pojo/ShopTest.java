a121d7ee92e0aa63189561bfde911d2c353a8006
alex3 <alexprocards@mail.ru>
2022-02-14 13:51:43 +0500

    3.1.Массив с пустыми ячейками [#176470 #264388]

--- src/test/java/ru/job4j/pojo/ShopTest.java
+++ src/test/java/ru/job4j/pojo/ShopTest.java
@@ -10,7 +10,7 @@ public class ShopTest {
     public void whenLastNull() {
         Product[] products = new Product[5];
         products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
+        products[1] = new Product("Bread", 5);
         products[2] = new Product("Egg", 19);
         int rsl = Shop.indexOfNull(products);
         assertThat(rsl, is(3));
