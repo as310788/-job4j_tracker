017971dafce63657d74dd0fedbdf1b3ea87ac706
alex3 <alexprocards@mail.ru>
2022-03-02 18:26:30 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,8 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        String createdFormat = formatter.format(item.getCreated());
-        System.out.println("Текущие дата и время после форматирования: " + createdFormat);
+        System.out.println(item);
 }
 }
