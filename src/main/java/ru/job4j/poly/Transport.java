fd47a386690b015ebb5b9314418e90d6eef19cce
alex3 <alexprocards@mail.ru>
2022-03-08 23:13:58 +0500

    3. Интерфейс Input [#181092 #272563]

--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -1,9 +1,9 @@
 package ru.job4j.poly;
 
 public interface Transport {
-    String drive();
+    void drive();
 
-    int passengers(int unit);
+    void passengers(int unit);
 
     double oil(double galon, double price);
 }
