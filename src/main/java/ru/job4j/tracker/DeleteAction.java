b31bf4c6eea5c83a8ec2be5fe9308e74a0453ee2
alex3 <alexprocards@mail.ru>
2022-03-25 11:42:57 +0500

    8. Реализация меню за счет шаблона стратегия. [#181780 #279628]

--- src/main/java/ru/job4j/tracker/DeleteAction.java
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -3,11 +3,17 @@ package ru.job4j.tracker;
 public class DeleteAction implements UserAction {
     @Override
     public String name() {
-        return null;
+        return "Deleted item";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        return false;
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        if (tracker.delete(id)) {
+            System.out.println("Заявка удалена успешно.");
+        } else {
+            System.out.println("Ошибка удаления заявки.");
+        }
+        return true;
     }
 }
