322389f0f40ff6aed3a038add7186709247b6dd2
alex3 <alexprocards@mail.ru>
2022-03-29 11:35:36 +0500

    9.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/DeleteAction.java
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class DeleteAction implements UserAction {
+    private final Output out;
+
+    public DeleteAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Deleted item";
@@ -10,9 +16,9 @@ public class DeleteAction implements UserAction {
     public boolean execute(Input input, Tracker tracker) {
         int id = Integer.parseInt(input.askStr("Enter id: "));
         if (tracker.delete(id)) {
-            System.out.println("Заявка удалена успешно.");
+            out.println("Заявка удалена успешно.");
         } else {
-            System.out.println("Ошибка удаления заявки.");
+            out.println("Ошибка удаления заявки.");
         }
         return true;
     }
