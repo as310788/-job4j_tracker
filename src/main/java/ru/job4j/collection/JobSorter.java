c92d798e5448fe14ff5c818ea9568dafc7f7cc58
alex3 <alexprocards@mail.ru>
2022-05-25 16:11:02 +0500

    2. Комбинированный компаратор [#10036]

---
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -0,0 +1,35 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class JobSorter {
+    public static void main(String[] args) {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        System.out.println(jobs);
+        Collections.sort(jobs);
+        System.out.println(jobs);
+        Collections.sort(jobs, new JobDescByNameLn());
+        System.out.println(jobs);
+        Collections.sort(jobs, new JobDescByName());
+        System.out.println(jobs);
+        Collections.sort(jobs, new JobDescByPriority());
+        System.out.println(jobs);
+        Collections.sort(jobs, new JobAscByName());
+        System.out.println(jobs);
+        Collections.sort(jobs, new JobAscByPriority());
+        System.out.println(jobs);
+        Comparator<Job> comb = new JobDescByNameLn()
+                .thenComparing(new JobDescByName())
+                .thenComparing(new JobDescByPriority());
+        Collections.sort(jobs, comb);
+        System.out.println(jobs);
+    }
+}
