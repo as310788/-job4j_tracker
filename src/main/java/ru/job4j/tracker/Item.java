bf91f035658d9c986c7d9df6586d9f9e5a6afe89
alex3 <alexprocards@mail.ru>
2022-05-25 13:47:13 +0500

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,7 +3,7 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Item {
+public class Item implements Comparable<Item> {
 
     private int id;
 
@@ -53,5 +53,10 @@ public class Item {
                 + ", name='" + name + '\''
                 + ", created=" + created.format(FORMATTER)
                 + '}';
+    }
+
+    @Override
+    public int compareTo(Item another) {
+        return Integer.compare(id, another.getId());
     }
 }
