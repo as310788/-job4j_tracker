2097b4595271d1403db2eb4d9b45c14ad7c78f60
alex3 <alexprocards@mail.ru>
2022-05-26 20:36:18 +0500

    0. Сортировка [#10096 #305003]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,31 +2,26 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item implements Comparable<Item> {
 
     private int id;
-
     private String name;
-
     private LocalDateTime created = LocalDateTime.now();
-
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
+
     }
 
     public Item(String name) {
         this.name = name;
     }
 
-    public Item(int id, String name) {
-        this.id = id;
+    public Item(String name, int id) {
         this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
+        this.id = id;
     }
 
     public int getId() {
@@ -38,12 +33,15 @@ public class Item implements Comparable<Item> {
     }
 
     public String getName() {
-
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     @Override
@@ -57,6 +55,23 @@ public class Item implements Comparable<Item> {
 
     @Override
     public int compareTo(Item another) {
-        return Integer.compare(id, another.getId());
+        return Integer.compare(id, another.id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
