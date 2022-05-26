a01319a6ea54cf3d64d062fe694ac737b6d6be36
alex3 <alexprocards@mail.ru>
2022-05-26 20:38:04 +0500

    0. Сортировка [#10096 #305003]

--- src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -1,22 +1,26 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class ItemAscByNameTest {
 
     @Test
     public void whenSortByNameItemAToZ() {
         List<Item> items = Arrays.asList(
-                new Item(2, "Petr"),
-                new Item(3, "Ivan"),
-                new Item(1, "Alex")
+                new Item("Petr", 2),
+                new Item("Ivan", 3),
+                new Item("Alex", 1)
         );
         Collections.sort(items, new ItemAscByName());
-        assertThat(items.get(0).getName(), is("Alex"));
+        List<Item> expected = Arrays.asList(
+                new Item("Alex", 1),
+                new Item("Ivan", 3),
+                new Item("Petr", 2)
+        );
+        Assert.assertEquals(items, expected);
     }
 }
