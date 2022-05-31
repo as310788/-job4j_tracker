0fc513aaed122a24fccc09e6f33ab3a03542ada8
alex3 <alexprocards@mail.ru>
2022-05-31 22:36:06 +0500

    2. Отсортировать департаменты [#34352 #307403]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -12,9 +12,8 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                start += el;
-                tmp.add(start);
-                start += "/";
+                tmp.add(start + el);
+                start += el + "/";
 
             }
         }
