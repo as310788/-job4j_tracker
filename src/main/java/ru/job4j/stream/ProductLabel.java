775c3c65407e52bef5d94f9399c4d9a6d7935405
alex3 <alexprocards@mail.ru>
2022-07-15 00:07:30 +0500

    0.2. Перенос методов в Stream API [#279131]

--- src/main/java/ru/job4j/stream/ProductLabel.java
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-
 public class ProductLabel {
 
     public List<String> generateLabels(List<Product> products) {
